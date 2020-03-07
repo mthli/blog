123bf00b326ac968951a19d6fee82eefa22849ac
Matthew Lee <matthewlee0725@gmail.com>
2020-03-07 18:42:01 +0800

    feat: not limit img width

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,18 +34,13 @@ module.exports = {
             },
           },
           {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
           `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-images`,
           `gatsby-remark-prismjs`,
           `gatsby-remark-smartypants`,
         ],
