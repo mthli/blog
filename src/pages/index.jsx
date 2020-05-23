71a36478e753ee87e5262771c1891226db08cfa6
Matthew Lee <matthewlee0725@gmail.com>
2020-05-23 13:18:59 +0800

    feat: change link underline style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ const BlogIndex = ({ data }) => {
                   marginBottom: rhythm(1 / 4),
                 }}
               >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
+                <Link to={node.fields.slug}>
                   {title}
                 </Link>
               </h3>
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,14 @@ import Wordpress2015 from 'typography-theme-wordpress-2015'
 
 Wordpress2015.overrideThemeStyles = () => ({
   a: {
-    color: 'var(--link-color)',
+    borderBottom: '1px solid var(--link-color)',
+    boxShadow: 'inset 0 -7px 0 var(--link-color)',
+    color: 'inherit',
+    textDecoration: 'none',
+    transition: 'all .25s ease-in-out',
+  },
+  'a:hover': {
+    backgroundColor: 'var(--link-color)',
   },
   'a.gatsby-resp-image-link': {
     boxShadow: 'none',
