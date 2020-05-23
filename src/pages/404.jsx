0f52f7edcce8c8c442ffb9c15d53eb43cfbcec46
Matthew Lee <matthewlee0725@gmail.com>
2020-05-23 13:37:59 +0800

    chore: clean code

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import Bio from '../components/bio'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404" />
-    <Bio />
     <h1>404</h1>
     <p>
       这里什么也没有，哈哈
