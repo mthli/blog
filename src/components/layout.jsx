f98bd2b05e55174868b8f8e35e3a32bce498e12a
Matthew Lee <matthewlee0725@gmail.com>
2020-05-27 21:33:58 +0800

    feat: adjust blockquote style

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,20 +3,18 @@ import React from 'react'
 import Bio from './bio'
 import { rhythm } from '../utils/typography'
 
-const Layout = ({ children }) => {
-  return (
-    <div
-      style={{
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        maxWidth: rhythm(26),
-        padding: `${rhythm(2)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <Bio />
-      <main>{children}</main>
-    </div>
-  )
-}
+const Layout = ({ children }) => (
+  <div
+    style={{
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      maxWidth: rhythm(26),
+      padding: `${rhythm(2)} ${rhythm(3 / 4)}`,
+    }}
+  >
+    <Bio />
+    <main>{children}</main>
+  </div>
+)
 
 export default Layout
