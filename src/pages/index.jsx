f75b937f2da7a38d647d1c898d61e6894b2866b9
Matthew Lee <matthewlee0725@gmail.com>
2020-10-14 22:47:16 +0800

    refactor: index compact style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,27 +14,31 @@ const BlogIndex = ({ data }) => {
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
+          <article
+            key={node.fields.slug}
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'baseline',
+              marginBottom: rhythm(1 / 2),
+            }}
+          >
+            <h3
+              style={{
+                margin: `0 ${rhythm(1 / 4)} 0 0`,
+              }}
+            >
+              <Link to={node.fields.slug}>
+                {title}
+              </Link>
+            </h3>
+            <div style={{
+              margin: 0,
+              color: 'hsla(0, 0%, 0%, 0.59)',
+            }}
+            >
+              {node.frontmatter.date}
+            </div>
           </article>
         )
       })}
@@ -54,14 +58,12 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          excerpt
           fields {
             slug
           }
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
             title
-            description
           }
         }
       }
